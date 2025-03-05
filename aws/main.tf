6b8e42bdba410468a7264ced39abecc84b049494
okki-0417 <ochan0901@gmail.com>
2025-03-06 01:02:26 +0900

    finish aws tutorial

--- aws/main.tf
+++ aws/main.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
 
-  required_versions = ">= 1.2.0"
+  required_version = ">= 1.2.0"
 }
 
 provider "aws" {
@@ -14,10 +14,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-08d70e59c07c61a3a"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = var.instance_name
   }
 }
